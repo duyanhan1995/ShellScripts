70296e4a5f716b84e73ce6c3bc24e34098443429
Yanhan <duyanhan1995@gmail.com>
2019-02-19 17:10:57 +0800

    Create JDK.sh

---
+++ JDK.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#卸载OpenJDK，并安装OracleJDK
+yum -y remove $(rpm -qa | grep jdk)
+wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+mkdir /usr/lib/jvm
+tar -xzf jdk-8u131-linux-x64.tar.gz -C /usr/lib/jvm
+echo '#设置JDK环境变量' >> /etc/profile
+echo 'export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_131' >> /etc/profile
+echo 'export CLASSPATH=$JAVA_HOME ' >> /etc/profile
+echo 'export PATH=$PATH:$JAVA_HOME/bin' >> /etc/profile
+sleep 2s
+source /etc/profile
+sleep 1s
+java -version
